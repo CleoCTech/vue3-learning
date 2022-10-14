fdec4d954a03ac8284145c171e0b55b41b67ce70
CleoCTech <cleophasomwenga292@gmail.com>
2022-10-14 09:14:06 +0300

    Episode 06 --Component Props

--- ReadMe.md
+++ ReadMe.md
@@ -26,3 +26,7 @@ Since we can have these many components, in the next episode we are going to ext
 -We have successfully extracted each Vue component to it's own file. And we can create these components as many as we can;
 -Using devtool extension in the browser, we can be able to see the nested components from rootapp and by selecting each component, we get to see it's reactivity data that's associated with. 
 -NB: For use to import this js component to our script section of out html, we use the `<script type="module">` otherwise it will throw an error. 
+
+#Episode 06: Component Props
+-Props are like parameters we pass when we create new instance of any component/class.
+-Which means, you have to pass these parameters when creating it's instance, but if we don't want to pass parameters, we can give default values incase someone does not pass. 
--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,12 +1,28 @@
 export default {
-    
-    template: `<button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing">
-        <slot/>
-    </button>`,
 
-    data(){
-        return {
-            processing: true,
+    template: `
+    <button 
+     :class="{
+        'border rounded px-5 py-2 disabled:cursor-not-allowed':true,
+        'bg-blue-600 hover:bg-gray-700': class_type == 'primary',
+        'bg-gray-200 hover:bg-gray-400': class_type == 'muted',
+        'bg-purple-200 hover:bg-gray-400': class_type == 'secondary',
+        'is-loading': processing,
+     }"
+     :disabled="processing"
+     >
+        <slot/>
+    </button>
+    `,
+
+    props: {
+        class_type: {
+            type:String,
+            default:'primary'
+        }, 
+        processing: {
+            type:Boolean,
+            default:false
         }
     }
 
